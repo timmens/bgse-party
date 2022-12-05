2709efa77821b426805bd2ac71be23510dc9f2de
Tim <tim@mensinger96.com>
2022-12-05 22:18:28 +0100

    Send to correct port

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
 
-console.log('Server running on http://localhost:3000');
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
